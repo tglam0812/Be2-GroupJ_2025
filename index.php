07c84f25cecf0d434358f76d8b5e2b91505e6d90
tglam0812 <trangialam2004@gmail.com>
2025-03-14 17:03:13 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include "header.php";
+?>
+<?php include "hero.php"; ?>
+<?php include "category.php"; ?>
+<?php include "featured.php"; ?>
+<?php include "product.php"; ?>
+<?php
+include "blog_footer.php";
+?>
+<?php
+include "footer.php"
+?>
