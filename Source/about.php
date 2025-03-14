adc2ce98ba55d0a5f8b1d5ef053150853f378bbc
tglam0812 <trangialam2004@gmail.com>
2025-03-14 17:29:32 +0700

    Create file about.php login.php, gioi thieu ve web

---
+++ Source/about.php
@@ -0,0 +1,45 @@
+<!-- Header -->
+<?php include "header.php"; ?>
+<!-- End Header -->
+
+<!-- Breadcrumb Section Begin -->
+<section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="breadcrumb__text">
+                    <h2>Giới thiệu</h2>
+                    <div class="breadcrumb__option">
+                        <a href="./index.php">Trang chủ</a>
+                        <span>Giới thiệu</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Breadcrumb Section End -->
+
+<section class="about-us">
+    <div class="container">
+        <h1>Giới thiệu</h1>
+        <p>Chào mừng đến với cửa hàng của chúng tôi! Chúng tôi cung cấp các loại thiết bị điện tử thông minh, hiện đại, siêu mượt, chất lượng để phục nhu cầu của các bạn.</p>
+
+        <h2>Chúng Tôi Cung Cấp Gì?</h2>
+        <ul>
+            <li>Những thiết bị điện tử của các hãng: Apple, SamSung, ViVo, Acer, Oppo, Dell.</li>
+            <li>Cung cấp các thiệt bị từ những năm trước cho đến hiện tại như: Iphone7,8...</li>
+            <li>Quà tặng đi kèm: ốp lưng điện thoại, kính cường lực, khăn lau điện thoại.</li>
+            <li>Các sản phẩm đặc biệt: điện thoại có màn hình gập, hiệu suất pin cao, thời gian sử dụng lâu.</li>
+        </ul>
+
+        <h2>Tại Sao Chọn Chúng Tôi?</h2>
+        <p>Chúng tôi cam kết mang lại cho bạn những sản phẩm tuyệt vời, an toàn và có nguồn gốc rõ ràng. Với đội ngũ nhân viên chuyên nghiệp, dịch vụ khách hàng tận tâm, chúng tôi hy vọng sẽ đáp ứng được mọi nhu cầu của bạn.</p>
+
+        <h2>Chúng Tôi Là Ai?</h2>
+        <p>Chúng tôi là một đội ngũ đam mê các thiết bị công nghệ hiện đại phát triển, luôn tìm kiếm những sản phẩm tốt nhất và mang lại giá trị cho khách hàng. Mỗi ngày, chúng tôi không ngừng cải tiến chất lượng sản phẩm và dịch vụ của mình.</p>
+    </div>
+</section>
+<!-- Footer -->
+<?php include "footer.php" ?>
+<!-- End Footer -->
